6a4ed9a08cebb3b31b8146e0392d157937ce82a6
Duc <vanduc2514@gmail.com>
2021-04-24 17:49:22 +0700

    Try for-loop in order to get the combination from an array.

--- src/App.java
+++ src/App.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         int[] input = new int[]{1, 2, 3, 4, 5};
-        List<int[]> result = new ArrayList<>();
-        for (int[] combination : combinationList(input, 0, result)) {
-            System.out.println(Arrays.toString(combination));
+        for (int i = 0; i < input.length; i++) {
+            int[] output = new int[3];
+            output[0] = input[i];
+            for (int j = i + 1; j < input.length; j++) {
+                output[1] = input[j];
+                for (int k = j + 1; k < input.length; k++) {
+                    output[2] = input[k];
+                    System.out.println(Arrays.toString(output));
+                }
+            }
         }
-    }
-
-    public static List<int[]> combinationList(int[] input, int rootIndex, List<int[]> listInt) {
-        if (rootIndex == input.length - 1) {
-            return listInt;
-        }
-        for (int i = rootIndex + 1; i < input.length; i++) {
-            int[] combination = new int[2];
-            combination[0] = input[rootIndex];
-            combination[1] = input[i];
-            listInt.add(combination);
-        }
-        int root = ++rootIndex;
-        return combinationList(input, root, listInt);
     }
 }
